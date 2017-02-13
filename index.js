ce0801769b4d4289e9409d639b132c123ff9dea5
Mohammad Noh Iskandar Jamaluddin <anatraxia@gmail.com>
2017-02-13 16:37:12 +0800

    update controller

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const mongoose = require('mongoose')
-const todosController = require('./controllers/todos_controller')
-
-mongoose.connect('mongodb://localhost/todo-list')
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://sa:password@ds149059.mlab.com:49059/todolistdb')
 mongoose.Promise = global.Promise
 
-// TODO. include express and body-parser, plugin in the todos controller and start listening
+const bodyParser = require('body-parser')
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const todosController = require('./controllers/todos_controller')
+
+// startup api middleware, all req goes to controller
+app.use(bodyParser.json())
+
+// app.set('view engine', 'ejs')
+
+app.listen(port, function () {
+  console.log('Express is running ' + port)
+})
+
+app.use('/todos', todosController)
